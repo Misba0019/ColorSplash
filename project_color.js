5f53e5687b2c0050d417cf5b40fa7449372d0eb0
Misba Ali <misbaalikhan192@gmail.com>
2025-03-18 23:57:48 +0530

    Deleted and updated comments

--- project_color.js
+++ project_color.js
@@ -14,9 +14,5 @@ const randomColor = () => {
     return `rgb(${r}, ${g}, ${b})`; 
 }
 
-//Step 1: So, in here we made a seperate function that works for bringing random colors through rgb() and Math. methods 
-
-//Step 2: Then we added that random color function to another function expression that is inside the Event Listener, 'click'
-//We then put inside the function expression, to document to get the body element,
-//inside the body element, we put style and then background-color will be updated to the randomColor function expression.
-//And h1 element's innerText will be updated to the randomColor text
+// Generates a random RGB color
+// Updates background color and h1 text on button click
